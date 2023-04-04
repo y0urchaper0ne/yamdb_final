9757cdab65ebf3c6c7e97431315814668e1f8a09
Илья Никитин <ilyanikitin2308@gmail.com>
2023-04-04 19:08:53 +0300

    workflow upd

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -14,7 +14,9 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '*',
+    '51.250.4.142']
 
 
 # Application definition
